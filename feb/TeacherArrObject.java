c418d02cf633db5bf5eb85b0494b79bff7bffe29
Hibanasrin <hibanasrin448@gmail.com>
2025-04-09 15:23:42 +0530

    Add files via upload

---
+++ feb/TeacherArrObject.java
@@ -0,0 +1,14 @@
+package MODULE_3;
+
+public class TeacherArrObject {
+
+	public TeacherArrObject() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
